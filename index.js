8f34d4891c68a427f927d90d068a9ebf73f6a86b
biniter1 <hasonbin123@gmail.com>
2025-09-29 16:45:01 +0700

    testcase1

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello, CI/CD World!');
 });
+// TEST CASE: Lỗ hổng Command Injection
+app.get('/network-lookup', (req, res) => {
+  const host = req.query.host;
 
+  // Dòng code này rất nguy hiểm vì nó thực thi trực tiếp input của người dùng
+  exec('nslookup ' + host, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.status(500).send('An error occurred');
+    }
+    res.send(`<pre>${stdout}</pre>`);
+  });
+});
 // Xuất app để file test có thể sử dụng
 module.exports = app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
